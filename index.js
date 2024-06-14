92383992602bd053ab1ab05655919cf0a6169bb2
syntax-co <51330734+syntax-co@users.noreply.github.com>
2024-06-13 19:13:43 -0500

    final

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
 require('dotenv').config()
 
 var app = express();
@@ -7,11 +8,42 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+       cb(null, 'public/uploads')
+ },
+  filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+
+const upload = multer({ storage: storage });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+function intToBytes(int) {
+  let byteArray = new Uint8Array(4);
+  for (let i = 0; i < 4; i++) {
+      byteArray[i] = (int >> (8 * (3 - i))) & 0xFF;
+  }
+  return byteArray;
+}
 
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res) => {
+  
+  const fileData = req.file
+  
+
+  const final = {
+    name:fileData.originalname,
+    type:fileData.mimetype,
+    size:fileData.size
+  } 
+
+  res.json(final)
+})
 
 
 const port = process.env.PORT || 3000;
